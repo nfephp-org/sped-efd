f247ca73de2ee37f949fd80a108798057011e367
robmachado <linux.rlm@gmail.com>
2017-03-01 11:09:33 -0300

    [UPD] Inicio estruturação das classes para EFD fiscal

---
+++ src/Blocos/BlocoInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace NFePHP\EFD\Blocos;
+
+interface BlocoInterface
+{
+    public function __toString();
+}
---
+++ src/EFD.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace NFePHP\EFD;
+
+use NFePHP\EFD\Blocos\BlocoInterface;
+use NFePHP\EFD\Bloco;
+
+class EFD
+{
+    public $B000 = [];
+    public $B001 = [];
+    public $B005 = [];
+    public $B100 = [];
+    public $B150 = [];
+    public $B190 = [];
+    public $B200 = [];
+    public $B400 = [];
+    public $B460 = [];
+    public $B990 = [];
+    public $C001 = [];
+    public $C100 = [];
+    public $C120 = [];
+    public $C140 = [];
+    public $C141 = [];
+    public $C170 = [];
+    public $C190 = [];
+    public $C195 = [];
+    public $C990 = [];
+}
