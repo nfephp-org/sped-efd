c0c23f5ca900df8ee450073f2ce07dfe35cc2c1c
robmachado <linux.rlm@gmail.com>
2018-09-12 16:48:22 -0300

    [UPD] +classes

---
+++ src/Blocks/ElementsCont/B0400.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace NFePHP\EFD\Blocks\ElementsCont;
+
+use NFePHP\EFD\Blocks\ElementBase;
+use NFePHP\EFD\Blocks\ElementInterface;
+use \stdClass;
+
+class B0400 extends ElementBase implements ElementInterface
+{
+    const REG = '0400';
+    const LEVEL = 0;
+    const PARENT = '';
+    
+    protected $parameters = [];
+    
+    /**
+     * Constructor
+     * @param \stdClass $std
+     */
+    public function __construct(\stdClass $std)
+    {
+        parent::__construct();
+        $this->std = $this->standarize($std, self::REG);
+    }
+
+    /**
+     * Retorna o elemento formatado em uma string
+     * @return string
+     */
+    public function __toString()
+    {
+        return '|' . self::REG . '|' . $this->build();
+    }
+}
