3cccaa11b527ed20b804b24b65a5446b5e35d1d1
robmachado <linux.rlm@gmail.com>
2018-09-21 10:04:13 -0300

    [UPD] elementos do Bloco E

---
+++ src/Elements/ICMSIPI/E111.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace NFePHP\EFD\Elements\ICMSIPI;
+
+use NFePHP\EFD\Common\Element;
+use NFePHP\EFD\Common\ElementInterface;
+use \stdClass;
+
+class E111 extends Element implements ElementInterface
+{
+    const REG = 'E111';
+    const LEVEL = 0;
+    const PARENT = '';
+
+    protected $parameters = [];
+
+    /**
+     * Constructor
+     * @param \stdClass $std
+     */
+    public function __construct(\stdClass $std)
+    {
+        parent::__construct(self::REG);
+        $this->std = $this->standarize($std);
+    }
+}
