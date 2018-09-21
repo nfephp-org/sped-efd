82ffc91b4b60c8386a227ac52d5f827f134a478f
robmachado <linux.rlm@gmail.com>
2018-09-21 11:09:43 -0300

    [UPD] Elementos do Bloco G

--- src/Blocks/ICMSIPI/BlockG.php
+++ src/Blocks/ICMSIPI/BlockG.php
@@ -16,7 +16,14 @@ final class BlockG extends Block implements BlockInterface
 {
     const TOTAL = 'G990';
     
-    public $elements = [];
+    public $elements = [
+        'G001' => ['class' => Elements\G001::class, 'level' => 1, 'type' => 'single'],
+        'G110' => ['class' => Elements\G110::class, 'level' => 2, 'type' => 'multiple'],
+        'G125' => ['class' => Elements\G125::class, 'level' => 3, 'type' => 'multiple'],
+        'G126' => ['class' => Elements\G126::class, 'level' => 4, 'type' => 'multiple'],
+        'G130' => ['class' => Elements\G130::class, 'level' => 4, 'type' => 'multiple'],
+        'G140' => ['class' => Elements\G140::class, 'level' => 5, 'type' => 'multiple']
+    ];
     
     public function __construct()
     {
---
+++ src/Elements/ICMSIPI/G140.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace NFePHP\EFD\Elements\ICMSIPI;
+
+use NFePHP\EFD\Common\Element;
+use NFePHP\EFD\Common\ElementInterface;
+use \stdClass;
+
+class G140 extends Element implements ElementInterface
+{
+    const REG = 'G140';
+    const LEVEL = 5;
+    const PARENT = '';
+
+    protected $parameters = [];
+
+    /**
+     * Constructor
+     * @param \stdClass $std
+     */
+    public function __construct(\stdClass $std)
+    {
+        parent::__construct(self::REG);
+        $this->std = $this->standarize($std);
+    }
+}
