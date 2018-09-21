77facfc439027b10e0ecd7a8a85f29b3225da7be
robmachado <linux.rlm@gmail.com>
2018-09-21 11:02:31 -0300

    [UPD] elementos Bloco D

---
+++ src/Elements/ICMSIPI/D590.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace NFePHP\EFD\Elements\ICMSIPI;
+
+use NFePHP\EFD\Common\Element;
+use NFePHP\EFD\Common\ElementInterface;
+use \stdClass;
+
+class D590 extends Element implements ElementInterface
+{
+    const REG = 'D590';
+    const LEVEL = 3;
+    const PARENT = '';
+
+    protected $parameters = [];
+
+    /**
+     * Constructor
+     * @param \stdClass $std
+     */
+    public function __construct(\stdClass $std)
+    {
+        parent::__construct(self::REG);
+        $this->std = $this->standarize($std);
+    }
+}
