bb2fd5b3af0fae9bc5823d4cc260775ce46a833e
eduardorcsouza <eduardo.r.c.souza@gmail.com>
2018-09-24 15:19:46 -0300

    [UPD] Elementos C111 do bloco C #76

--- src/Elements/ICMSIPI/C111.php
+++ src/Elements/ICMSIPI/C111.php
@@ -6,13 +6,32 @@ use NFePHP\EFD\Common\Element;
 use NFePHP\EFD\Common\ElementInterface;
 use \stdClass;
 
+/**
+ * REGISTRO C111: PROCESSO REFERENCIADO
+ * @package NFePHP\EFD\Elements\ICMSIPI
+ */
 class C111 extends Element implements ElementInterface
 {
     const REG = 'C111';
     const LEVEL = 4;
-    const PARENT = '';
+    const PARENT = 'C110';
 
-    protected $parameters = [];
+    protected $parameters = [
+        'NUM_PROC' => [
+            'type' => 'string',
+            'regex' => '^[A-z0-9]{1,15}$',
+            'required' => true,
+            'info' => 'Identificação do processo ou ato concessório',
+            'format' => ''
+        ],
+        'IND_PROC' => [
+            'type' => 'string',
+            'regex' => '^(0|1|2|3|9)$',
+            'required' => true,
+            'info' => 'Indicador da origem do processo',
+            'format' => ''
+        ]
+    ];
 
     /**
      * Constructor
