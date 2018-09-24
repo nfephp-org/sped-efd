b8421d15ae9a340e963ddaa09483524091e42df5
eduardorcsouza <eduardo.r.c.souza@gmail.com>
2018-09-24 15:06:46 -0300

    [UPD] Elementos C110 do bloco C #75

--- src/Elements/ICMSIPI/C110.php
+++ src/Elements/ICMSIPI/C110.php
@@ -6,13 +6,32 @@ use NFePHP\EFD\Common\Element;
 use NFePHP\EFD\Common\ElementInterface;
 use \stdClass;
 
+/**
+ * REGISTRO C110: INFORMAÇÃO COMPLEMENTAR DA NOTA FISCAL (CÓDIGO 01, 1B, 04 e 55).
+ * @package NFePHP\EFD\Elements\ICMSIPI
+ */
 class C110 extends Element implements ElementInterface
 {
     const REG = 'C110';
     const LEVEL = 3;
-    const PARENT = '';
+    const PARENT = 'C100';
 
-    protected $parameters = [];
+    protected $parameters = [
+        'COD_INF' => [
+            'type' => 'string',
+            'regex' => '^[A-z0-9]{1,6}$',
+            'required' => true,
+            'info' => 'Código da informação complementar do documento fiscal',
+            'format' => ''
+        ],
+        'TXT_COMPL' => [
+            'type' => 'string',
+            'regex' => '^(.*)$',
+            'required' => true,
+            'info' => 'Descrição complementar do código de referência.',
+            'format' => ''
+        ]
+    ];
 
     /**
      * Constructor
