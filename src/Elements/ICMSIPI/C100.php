f68cc2b20ef8efb593fc50ac496ab75a44814480
eduardorcsouza <eduardo.r.c.souza@gmail.com>
2018-09-24 14:22:18 -0300

    [UPD] Elementos C101 do bloco C #73

--- src/Elements/ICMSIPI/C100.php
+++ src/Elements/ICMSIPI/C100.php
@@ -7,6 +7,11 @@ use NFePHP\EFD\Common\Element;
 use NFePHP\EFD\Common\ElementInterface;
 use \stdClass;
 
+/**
+ * REGISTRO C100: NOTA FISCAL (CÓDIGO 01), NOTA FISCAL AVULSA (CÓDIGO 1B),
+ * NOTA FISCAL DE PRODUTOR (CÓDIGO 04), NF-e (CÓDIGO 55) e NFC-e (CÓDIGO 65).
+ * @package NFePHP\EFD\Elements\ICMSIPI
+ */
 class C100 extends Element implements ElementInterface
 {
     const REG = 'C100';
@@ -220,6 +225,7 @@ class C100 extends Element implements ElementInterface
     {
         parent::__construct(self::REG);
         $this->std = $this->standarize($std);
+        $this->postValidation();
     }
 
     public function postValidation()
--- src/Elements/ICMSIPI/C101.php
+++ src/Elements/ICMSIPI/C101.php
@@ -6,13 +6,41 @@ use NFePHP\EFD\Common\Element;
 use NFePHP\EFD\Common\ElementInterface;
 use \stdClass;
 
+/**
+ * REGISTRO C101: INFORMAÇÃO COMPLEMENTAR DOS DOCUMENTOS FISCAIS
+ * QUANDO DAS OPERAÇÕES INTERESTADUAIS DESTINADAS A CONSUMIDOR FINAL
+ * NÃO CONTRIBUINTE EC 87/15 (CÓDIGO 55)
+ * @package NFePHP\EFD\Elements\ICMSIPI
+ */
 class C101 extends Element implements ElementInterface
 {
     const REG = 'C101';
     const LEVEL = 3;
-    const PARENT = '';
+    const PARENT = 'C100';
 
-    protected $parameters = [];
+    protected $parameters = [
+        'VL_FCP_UF_DEST' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => false,
+            'info' => 'Valor total relativo ao Fundo de Combate à Pobreza (FCP) da UF de destino',
+            'format' => ''
+        ],
+        'VL_ICMS_UF_DEST' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => false,
+            'info' => 'Valor total do ICMS Interestadual para a UF de destino',
+            'format' => ''
+        ],
+        'VL_ICMS_UF_REM' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => false,
+            'info' => 'Valor total do ICMS Interestadual para a UF do remetente',
+            'format' => ''
+        ],
+    ];
 
     /**
      * Constructor
