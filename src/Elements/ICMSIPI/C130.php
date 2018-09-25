6d20b1e40ead614cc1280a31de6cb85f6ba87c8e
eduardorcsouza <eduardo.r.c.souza@gmail.com>
2018-09-25 13:25:45 -0300

    [UPD] Elemento C130 do bloco C #114

--- src/Elements/ICMSIPI/C130.php
+++ src/Elements/ICMSIPI/C130.php
@@ -10,9 +10,59 @@ class C130 extends Element implements ElementInterface
 {
     const REG = 'C130';
     const LEVEL = 3;
-    const PARENT = '';
+    const PARENT = 'C100';
 
-    protected $parameters = [];
+    protected $parameters = [
+        'VL_SERV_NT' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => true,
+            'info' => 'Valor dos serviços sob não-incidência ou nãotributados pelo ICMS',
+            'format' => '15v2'
+        ],
+        'VL_BC_ISSQN' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => true,
+            'info' => 'Valor da base de cálculo do ISSQN',
+            'format' => '15v2'
+        ],
+        'VL_ISSQN' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => false,
+            'info' => 'Valor do ISSQN',
+            'format' => '15v2'
+        ],
+        'VL_BC_IRRF' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => false,
+            'info' => 'Valor da base de cálculo do Imposto de Renda Retido na Fonte',
+            'format' => '15v2'
+        ],
+        'VL_ IRRF' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => false,
+            'info' => 'Valor do Imposto de Renda - Retido na Fonte',
+            'format' => '15v2'
+        ],
+        'VL_BC_PREV' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => false,
+            'info' => 'Valor da base de cálculo de retenção da Previdência Social',
+            'format' => '15v2'
+        ],
+        'VL_PREV' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => false,
+            'info' => 'Valor destacado para retenção da Previdência Social',
+            'format' => '15v2'
+        ],
+    ];
 
     /**
      * Constructor
