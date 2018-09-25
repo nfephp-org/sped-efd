7f2e4b69aa6df22996283e5e42753affe04e4c43
eduardorcsouza <eduardo.r.c.souza@gmail.com>
2018-09-25 13:25:09 -0300

    [UPD] Elemento C140 do bloco C #115

--- src/Elements/ICMSIPI/C140.php
+++ src/Elements/ICMSIPI/C140.php
@@ -10,9 +10,53 @@ class C140 extends Element implements ElementInterface
 {
     const REG = 'C140';
     const LEVEL = 3;
-    const PARENT = '';
+    const PARENT = 'C100';
 
-    protected $parameters = [];
+    protected $parameters = [
+        'IND_EMIT' => [
+            'type' => 'string',
+            'regex' => '^[0-1]{1}$',
+            'required' => true,
+            'info' => 'Indicador do emitente do documento fiscal',
+            'format' => ''
+        ],
+        'IND_TIT' => [
+            'type' => 'string',
+            'regex' => '^(01|02|03|99)$',
+            'required' => true,
+            'info' => 'Indicador do tipo de título de crédito',
+            'format' => ''
+        ],
+        'DESC_TIT' => [
+            'type' => 'string',
+            'regex' => '^(.*)$',
+            'required' => false,
+            'info' => 'Descrição complementar do título de crédito',
+            'format' => ''
+        ],
+        'NUM_TIT' => [
+            'type' => 'string',
+            'regex' => '^(.*)$',
+            'required' => true,
+            'info' => 'Número ou código identificador do título de crédito',
+            'format' => ''
+        ],
+        'QTD_PARC' => [
+            'type' => 'integer',
+            'regex' => '^([0-9]{1,2})$',
+            'required' => true,
+            'info' => 'Quantidade de parcelas a receber/pagar',
+            'format' => ''
+        ],
+        'VL_TIT' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => true,
+            'info' => 'Valor total dos títulos de créditos',
+            'format' => '15v2'
+        ],
+
+    ];
 
     /**
      * Constructor
