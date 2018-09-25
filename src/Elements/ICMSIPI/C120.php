2e7a0579d4840f5b65905e60e4e75c715586f3f0
eduardorcsouza <eduardo.r.c.souza@gmail.com>
2018-09-25 13:25:29 -0300

    [UPD] Elemento C120 do bloco C #113

--- src/Elements/ICMSIPI/C120.php
+++ src/Elements/ICMSIPI/C120.php
@@ -6,13 +6,56 @@ use NFePHP\EFD\Common\Element;
 use NFePHP\EFD\Common\ElementInterface;
 use \stdClass;
 
+/**
+ * REGISTRO C120: COMPLEMENTO DE DOCUMENTO - OPERAÇÕES DE IMPORTAÇÃO (CÓDIGOS 01 e 55)
+ * Este registro tem por objetivo informar detalhes das operações de importação, que estejam sendo documentadas pela
+ * nota fiscal escriturada no registro C100, quando o campo IND_OPER for igual a “0” (zero),
+ * indicando operação de entrada.
+ * @package NFePHP\EFD\Elements\ICMSIPI
+ */
 class C120 extends Element implements ElementInterface
 {
     const REG = 'C120';
     const LEVEL = 3;
-    const PARENT = '';
+    const PARENT = 'C100';
 
-    protected $parameters = [];
+    protected $parameters = [
+        'COD_DOC_IMP' => [
+            'type' => 'string',
+            'regex' => '^(0|1)+$',
+            'required' => true,
+            'info' => 'Documento de importação',
+            'format' => ''
+        ],
+        'NUM_DOC_IMP' => [
+            'type' => 'string',
+            'regex' => '^([0-9]{12})+$',
+            'required' => true,
+            'info' => 'Número do documento de Importação',
+            'format' => ''
+        ],
+        'PIS_IMP' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => false,
+            'info' => 'Valor pago de PIS na importação',
+            'format' => '15v2'
+        ],
+        'COFINS_IMP' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => false,
+            'info' => 'Valor pago de COFINS na importação',
+            'format' => '15v2'
+        ],
+        'NUM_ACDRAW' => [
+            'type' => 'string',
+            'regex' => '^([0-9]{1,20})$',
+            'required' => false,
+            'info' => 'Número do Ato Concessório do regime Drawback',
+            'format' => ''
+        ],
+    ];
 
     /**
      * Constructor
