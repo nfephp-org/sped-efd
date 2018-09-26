346377e79d48337bc01f15b91628428c6eceb744
eduardorcsouza <eduardo.r.c.souza@gmail.com>
2018-09-26 15:54:03 -0300

    [UPD] Elemento C178 do bloco C #128

--- src/Elements/ICMSIPI/C178.php
+++ src/Elements/ICMSIPI/C178.php
@@ -6,13 +6,43 @@ use NFePHP\EFD\Common\Element;
 use NFePHP\EFD\Common\ElementInterface;
 use \stdClass;
 
+/**
+ * REGISTRO C178: OPERAÇÕES COM PRODUTOS SUJEITOS À TRIBUTAÇÃO DE IPI POR
+ * UNIDADE OU QUANTIDADE DE PRODUTO
+ * O registro tem por objetivo fornecer informações adicionais sobre os produtos cuja forma de tributação do IPI,
+ * fixada em reais, seja calculada por unidade ou por determinada quantidade de produto, conforme tabelas de classes de
+ * valores.
+ * @package NFePHP\EFD\Elements\ICMSIPI
+ */
 class C178 extends Element implements ElementInterface
 {
     const REG = 'C178';
     const LEVEL = 4;
-    const PARENT = '';
+    const PARENT = 'C170';
 
-    protected $parameters = [];
+    protected $parameters = [
+        'CL_ENQ' => [
+            'type' => 'string',
+            'regex' => '^.{0,5}$',
+            'required' => false,
+            'info' => 'Código da classe de enquadramento do IPI, conforme Tabela 4.5.1.',
+            'format' => ''
+        ],
+        'VL_UNID' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => false,
+            'info' => 'Valor por unidade padrão de tributação',
+            'format' => '15v2'
+        ],
+        'QUANT_PAD' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => false,
+            'info' => 'Quantidade total de produtos na unidade padrão de tributação',
+            'format' => '15v3'
+        ],
+    ];
 
     /**
      * Constructor
