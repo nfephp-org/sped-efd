d7f3633ca212a0c8dbd9ce3b52151ed3bc03d550
eduardorcsouza <eduardo.r.c.souza@gmail.com>
2018-09-26 15:51:28 -0300

    [UPD] Elemento C172 do bloco C #122

--- src/Elements/ICMSIPI/C172.php
+++ src/Elements/ICMSIPI/C172.php
@@ -6,13 +6,40 @@ use NFePHP\EFD\Common\Element;
 use NFePHP\EFD\Common\ElementInterface;
 use \stdClass;
 
+/**
+ * REGISTRO C172: OPERAÇÕES COM ISSQN (CÓDIGO 01)
+ * Este registro tem por objetivo informar dados da prestação de serviços
+ * @package NFePHP\EFD\Elements\ICMSIPI
+ */
 class C172 extends Element implements ElementInterface
 {
     const REG = 'C172';
     const LEVEL = 4;
-    const PARENT = '';
+    const PARENT = 'C170';
 
-    protected $parameters = [];
+    protected $parameters = [
+        'VL_BC_ISSQN' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => false,
+            'info' => 'Valor da base de cálculo do ISSQN',
+            'format' => '15v2'
+        ],
+        'ALIQ_ISSQN' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => false,
+            'info' => 'Alíquota do ISSQN',
+            'format' => '6v2'
+        ],
+        'VL_ISSQN' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => false,
+            'info' => 'Valor do ISSQN',
+            'format' => '15v2'
+        ],
+    ];
 
     /**
      * Constructor
