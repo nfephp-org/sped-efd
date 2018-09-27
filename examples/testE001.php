32842e2771c65a8e95f906b2259b6d6e21276842
Rauye Rogiski <rauyerogiski@duobox.com.br>
2018-09-27 14:44:57 -0300

    [UPD] Elementos E001 do bloco E #131

---
+++ examples/testE001.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+require_once '../bootstrap.php';
+
+use NFePHP\EFD\Elements\ICMSIPI\E001;
+
+$std = new stdClass();
+$std->IND_MOV = '0';
+
+try {
+    $b0 = new E001($std);
+    echo "{$b0}".'<br>';
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
+
+echo '|E001|0|<br>';
