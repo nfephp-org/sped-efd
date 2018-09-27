b891e53620153908fff72a789b683931fbaf6c59
Rauye Rogiski <rauyerogiski@duobox.com.br>
2018-09-27 15:21:07 -0300

    [UPD] Elementos E300 do bloco E #146

--- src/Elements/ICMSIPI/E300.php
+++ src/Elements/ICMSIPI/E300.php
@@ -9,10 +9,33 @@ use \stdClass;
 class E300 extends Element implements ElementInterface
 {
     const REG = 'E300';
-    const LEVEL = 0;
-    const PARENT = '';
+    const LEVEL = 2;
+    const PARENT = 'E001';
 
-    protected $parameters = [];
+    protected $parameters = [
+        'UF' => [
+            'type'     => 'string',
+            'regex'    => '^.{1,2}$',
+            'required' => true,
+            'info'     => 'Sigla da unidade da federação a que se refere à apuração '
+            .'do FCP e do ICMS Diferencial de Alíquota da UF de Origem/Destino',
+            'format'   => ''
+        ],
+        'DT_INI' => [
+            'type'     => 'integer',
+            'regex'    => '^(0[1-9]|[1-2][0-9]|31(?!(?:0[2469]|11))|30(?!02))(0[1-9]|1[0-2])([12]\d{3})$',
+            'required' => true,
+            'info'     => 'Data inicial a que a apuração se refere',
+            'format'   => ''
+        ],
+        'DT_FIN' => [
+            'type'     => 'integer',
+            'regex'    => '^(0[1-9]|[1-2][0-9]|31(?!(?:0[2469]|11))|30(?!02))(0[1-9]|1[0-2])([12]\d{3})$',
+            'required' => true,
+            'info'     => 'Data final a que a apuração se refere',
+            'format'   => ''
+        ]
+    ];
 
     /**
      * Constructor
