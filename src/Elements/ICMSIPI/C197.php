820c336eaff2a400452ba3c3456a977b783bb0ae
eduardorcsouza <eduardo.r.c.souza@gmail.com>
2018-09-27 09:30:51 -0300

    [UPD] Elemento C197 do bloco C #168

--- src/Elements/ICMSIPI/C197.php
+++ src/Elements/ICMSIPI/C197.php
@@ -10,9 +10,59 @@ class C197 extends Element implements ElementInterface
 {
     const REG = 'C197';
     const LEVEL = 4;
-    const PARENT = '';
+    const PARENT = 'C190';
 
-    protected $parameters = [];
+    protected $parameters = [
+        'COD_AJ' => [
+            'type' => 'string',
+            'regex' => '^.{10}$',
+            'required' => true,
+            'info' => 'Código do ajustes/benefício/incentivo, conforme tabela indicada no item 5.3.',
+            'format' => ''
+        ],
+        'DESCR_COMPL_AJ' => [
+            'type' => 'string',
+            'regex' => '^(.*)$',
+            'required' => false,
+            'info' => 'Descrição complementar do ajuste do documento fiscal',
+            'format' => ''
+        ],
+        'COD_ITEM' => [
+            'type' => 'string',
+            'regex' => '^.{0,60}$',
+            'required' => false,
+            'info' => 'Código do item (campo 02 do Registro 0200)',
+            'format' => ''
+        ],
+        'VL_BC_ICMS' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => false,
+            'info' => 'Base de cálculo do ICMS ou do ICMS ST',
+            'format' => '15v2'
+        ],
+        'ALIQ_ICMS' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => false,
+            'info' => 'Alíquota do ICMS',
+            'format' => '6v2'
+        ],
+        'VL_ICMS' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => false,
+            'info' => 'Valor do ICMS ou do ICMS ST',
+            'format' => '15v2'
+        ],
+        'VL_OUTROS' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => false,
+            'info' => 'Outros valores',
+            'format' => '15v2'
+        ],
+    ];
 
     /**
      * Constructor
