06681bc4fa0309f720a0d64a62ea73a2d747c559
eduardorcsouza <eduardo.r.c.souza@gmail.com>
2018-09-27 09:29:33 -0300

    [UPD] Elemento C190 do bloco C #165

--- src/Elements/ICMSIPI/C190.php
+++ src/Elements/ICMSIPI/C190.php
@@ -6,13 +6,97 @@ use NFePHP\EFD\Common\Element;
 use NFePHP\EFD\Common\ElementInterface;
 use \stdClass;
 
+/**
+ * REGISTRO C190: REGISTRO ANALÍTICO DO DOCUMENTO (CÓDIGO 01, 1B, 04, 55 e 65)
+ * Este registro tem por objetivo representar a escrituração dos documentos fiscais totalizados por CST, CFOP e
+ * Alíquota de ICMS.
+ * @package NFePHP\EFD\Elements\ICMSIPI
+ */
 class C190 extends Element implements ElementInterface
 {
     const REG = 'C190';
     const LEVEL = 3;
-    const PARENT = '';
+    const PARENT = 'C100';
 
-    protected $parameters = [];
+    protected $parameters = [
+        'CST_ICMS' => [
+            'type' => 'numeric',
+            'regex' => '^(\d{3})$',
+            'required' => true,
+            'info' => 'Código da Situação Tributária',
+            'format' => ''
+        ],
+        'CFOP' => [
+            'type' => 'numeric',
+            'regex' => '^(\d{3})$',
+            'required' => true,
+            'info' => 'Código Fiscal de Operação e Prestação do agrupamento de itens',
+            'format' => ''
+        ],
+        'ALIQ_ICMS' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => false,
+            'info' => 'Alíquota do ICMS',
+            'format' => '6v2'
+        ],
+        'VL_OPR' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => true,
+            'info' => 'Valor da operação na combinação de CST_ICMS, CFOP e alíquota do ICMS',
+            'format' => '15v2'
+        ],
+        'VL_BC_ICMS' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => true,
+            'info' => 'Parcela correspondente ao "Valor da base de cálculo do ICMS" referente à combinação de CST_ICMS, CFOP e alíquota do ICMS.',
+            'format' => '15v2'
+        ],
+        'VL_ICMS' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => true,
+            'info' => 'Parcela correspondente ao "Valor do ICMS", incluindo o FCP, quando aplicável, referente à combinação de CST_ICMS, CFOP e alíquota doICMS.',
+            'format' => '15v2'
+        ],
+        'VL_BC_ICMS_ST' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => true,
+            'info' => 'Parcela correspondente ao "Valor da base de cálculo do ICMS" da substituição tributária referente à combinação de CST_ICMS, CFOP e alíquota do ICMS.',
+            'format' => '15v2'
+        ],
+        'VL_ICMS_ST' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => true,
+            'info' => 'Parcela correspondente ao valor creditado/debitado do ICMS da substituição tributária, incluindo o FCP_ ST, quando aplicável, referente à combinação de CST_ICMS, CFOP, e alíquota do ICMS.',
+            'format' => '15v2'
+        ],
+        'VL_RED_BC' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => true,
+            'info' => 'Valor não tributado em função da redução da base de cálculo do ICMS, referente à combinação de CST_ICMS, CFOP e alíquota do ICMS.',
+            'format' => '15v2'
+        ],
+        'VL_IPI' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => true,
+            'info' => 'Parcela correspondente ao "Valor do IPI" referente à combinação CST_ICMS, CFOP e alíquota do ICMS.',
+            'format' => '15v2'
+        ],
+        'COD_OBS' => [
+            'type' => 'string',
+            'regex' => '^.{0,6}$',
+            'required' => false,
+            'info' => 'Código da observação do lançamento fiscal',
+            'format' => ''
+        ],
+    ];
 
     /**
      * Constructor
