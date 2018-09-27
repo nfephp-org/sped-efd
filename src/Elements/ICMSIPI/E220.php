102dbedcb49413425a8bb2842bcf916a687d2c0e
Rauye Rogiski <rauyerogiski@duobox.com.br>
2018-09-27 15:18:27 -0300

    [UPD] Elementos E220 do bloco E #142

--- src/Elements/ICMSIPI/E220.php
+++ src/Elements/ICMSIPI/E220.php
@@ -9,10 +9,33 @@ use \stdClass;
 class E220 extends Element implements ElementInterface
 {
     const REG = 'E220';
-    const LEVEL = 0;
-    const PARENT = '';
+    const LEVEL = 4;
+    const PARENT = 'E110';
 
-    protected $parameters = [];
+    protected $parameters = [
+        'COD_AJ_APUR' => [
+            'type'     => 'string',
+            'regex'    => '^.{8}$',
+            'required' => true,
+            'info'     => 'Código do ajuste da apuração e dedução, '
+            .'conforme a Tabela indicada no item 5.1.1',
+            'format'   => ''
+        ],
+        'DESCR_COMPL_AJ' => [
+            'type'     => 'string',
+            'regex'    => '^.*$',
+            'required' => false,
+            'info'     => 'Descrição complementar do ajuste da apuração',
+            'format'   => ''
+        ],
+        'VL_AJ_APUR' => [
+            'type'     => 'numeric',
+            'regex'    => '^\d+(\.\d*)?|\.\d+$',
+            'required' => true,
+            'info'     => 'Valor do ajuste da apuração',
+            'format'   => '15v2'
+        ]
+    ];
 
     /**
      * Constructor
