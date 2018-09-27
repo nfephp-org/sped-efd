a4f3f967221398e22098026b335d51f82ee1091e
Rauye Rogiski <rauyerogiski@duobox.com.br>
2018-09-27 15:15:02 -0300

    [UPD] Elementos E115 do bloco E #138

---
+++ examples/testE115.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+require_once '../bootstrap.php';
+
+use NFePHP\EFD\Elements\ICMSIPI\E115;
+
+$std = new stdClass();
+$std->COD_INF_ADIC = '12345678';
+$std->VL_INF_ADIC = 1800;
+$std->DESCR_COMPL_AJ = 'Descrição complementar do ajuste';
+
+try {
+    $b0 = new E115($std);
+    echo "{$b0}".'<br>';
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
+
+echo '|E115|12345678|1800|Descrição complementar do ajuste|<br>';
