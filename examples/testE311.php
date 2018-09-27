d78fc4ab27a4f5a58825ca7aaa81c9d3910addc5
Rauye Rogiski <rauyerogiski@duobox.com.br>
2018-09-27 15:22:27 -0300

    [UPD] Elementos E311 do bloco E #148

---
+++ examples/testE311.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+require_once '../bootstrap.php';
+
+use NFePHP\EFD\Elements\ICMSIPI\E311;
+
+$std = new stdClass();
+$std->COD_AJ_APUR = 'XX209999';
+$std->DESCR_COMPL_AJ = 'Descrição complementar do ajuste da apuração';
+$std->VL_AJ_APUR = 9.75;
+
+try {
+    $b0 = new E311($std);
+    echo "{$b0}".'<br>';
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
+
+echo '|E311|XX209999|Descrição complementar do ajuste da apuração|9.75|<br>';
