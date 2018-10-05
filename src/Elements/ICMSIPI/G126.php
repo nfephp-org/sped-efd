48dddc1bf0885adfa19d8145e796fabdb3f3eb7d
eduardorcsouza <eduardo.r.c.souza@gmail.com>
2018-10-05 14:47:57 -0300

    [UPD] Elementos do Bloco M de contribuições

--- src/Elements/ICMSIPI/G126.php
+++ src/Elements/ICMSIPI/G126.php
@@ -10,9 +10,72 @@ class G126 extends Element implements ElementInterface
 {
     const REG = 'G126';
     const LEVEL = 4;
-    const PARENT = '';
+    const PARENT = 'G!20';
 
-    protected $parameters = [];
+    protected $parameters = [
+        'DT_INI' => [
+            'type' => 'string',
+            'regex' => '^(0[1-9]|[1-2][0-9]|31(?!(?:0[2469]|11))|30(?!02))(0[1-9]|1[0-2])([12]\d{3})$',
+            'required' => true,
+            'info' => 'Data inicial do período de apuração ',
+            'format' => ''
+        ],
+        'DT_FIM' => [
+            'type' => 'string',
+            'regex' => '^(0[1-9]|[1-2][0-9]|31(?!(?:0[2469]|11))|30(?!02))(0[1-9]|1[0-2])([12]\d{3})$',
+            'required' => true,
+            'info' => 'Data final do período de apuração ',
+            'format' => ''
+        ],
+        'NUM_PARC' => [
+            'type' => 'numeric',
+            'regex' => '^(\d{1,3})$',
+            'required' => true,
+            'info' => 'Número da parcela do ICMS ',
+            'format' => ''
+        ],
+        'VL_PARC_PASS' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => true,
+            'info' => 'Valor da parcela de ICMS passível de apropriação - antes da aplicação da ' .
+                'totais ' .
+                'participação percentual do valor das saídas tributadas/exportação sobre as saídas ',
+            'format' => '15v2'
+        ],
+        'VL_TRIB_OC' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => true,
+            'info' => 'Valor do somatório das saídas tributadas e saídas para exportação no período ' .
+                'indicado neste registro ',
+            'format' => '15v2'
+        ],
+        'VL_TOTAL' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => true,
+            'info' => 'Valor total de saídas no período indicado neste registro ',
+            'format' => '15v2'
+        ],
+        'IND_PER_SAI' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => true,
+            'info' => 'Índice de participação do valor do somatório das saídas tributadas e saídas para ' .
+                'exportação no valor total de saídas (Campo 06 dividido pelo campo 07) ',
+            'format' => '15v8'
+        ],
+        'VL_PARC_APROP' => [
+            'type' => 'numeric',
+            'regex' => '^\d+(\.\d*)?|\.\d+$',
+            'required' => true,
+            'info' => 'Valor de outros créditos de ICMS a ser apropriado na apuração (campo 05 vezes o campo ' .
+                '08) ',
+            'format' => '15v2'
+        ],
+
+    ];
 
     /**
      * Constructor
--- src/Elements/ICMSIPI/G130.php
+++ src/Elements/ICMSIPI/G130.php
@@ -5,14 +5,70 @@ namespace NFePHP\EFD\Elements\ICMSIPI;
 use NFePHP\EFD\Common\Element;
 use NFePHP\EFD\Common\ElementInterface;
 use \stdClass;
+use NFePHP\Common\Keys;
 
 class G130 extends Element implements ElementInterface
 {
     const REG = 'G130';
     const LEVEL = 4;
-    const PARENT = '';
+    const PARENT = 'G100';
 
-    protected $parameters = [];
+    protected $parameters = [
+        'IND_EMIT' => [
+            'type' => 'string',
+            'regex' => '^(0|1)$',
+            'required' => true,
+            'info' => 'Indicador do emitente do documento fiscal ' .
+                ' 0- Emissão própria ' .
+                ' 1- Terceiros ',
+            'format' => ''
+        ],
+        'COD_PART' => [
+            'type' => 'string',
+            'regex' => '^.{1,60}$',
+            'required' => true,
+            'info' => 'Código do participante  ' .
+                ' - do emitente do documento ou do remetente das mercadorias, no caso de entradas ' .
+                ' - do adquirente, no caso de saídas ',
+            'format' => ''
+        ],
+        'COD_MOD' => [
+            'type' => 'string',
+            'regex' => '^.{2}$',
+            'required' => true,
+            'info' => 'Código do modelo de documento fiscal, conforme tabela 4.1.1 ',
+            'format' => ''
+        ],
+        'SERIE' => [
+            'type' => 'string',
+            'regex' => '^.{0,3}$',
+            'required' => false,
+            'info' => 'Série do documento fiscal ',
+            'format' => ''
+        ],
+        'NUM_DOC' => [
+            'type' => 'numeric',
+            'regex' => '^(\d{1,9})$',
+            'required' => true,
+            'info' => 'Número de documento fiscal ',
+            'format' => ''
+        ],
+        'CHV_NFE_CTE' => [
+            'type' => 'numeric',
+            'regex' => '^([0-9]{44})?$',
+            'required' => false,
+            'info' => 'Chave do documento fiscal eletrônico ',
+            'format' => ''
+        ],
+        'DT_DOC' => [
+            'type' => 'string',
+            'regex' => '^(0[1-9]|[1-2][0-9]|31(?!(?:0[2469]|11))|30(?!02))(0[1-9]|1[0-2])([12]\d{3})$',
+            'required' => true,
+            'info' => 'Data da emissão do documento fiscal ',
+            'format' => ''
+        ],
+
+    ];
 
     /**
      * Constructor
@@ -22,5 +78,15 @@ class G130 extends Element implements ElementInterface
     {
         parent::__construct(self::REG);
         $this->std = $this->standarize($std);
+        $this->postValidation();
+    }
+
+    public function postValidation()
+    {
+        if (!empty($this->std->chv_nfe_cte) and !Keys::isValid($this->std->chv_nfe_cte)) {
+            throw new \InvalidArgumentException("[" . self::REG . "] " .
+                " Dígito verificador incorreto no campo chave do " .
+                " campo CHV_NFE_CTE");
+        }
     }
 }
