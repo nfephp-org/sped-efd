0b58f9d365e99098cc39d8872b6bf3c29524cd39
robmachado <linux.rlm@gmail.com>
2019-01-16 11:47:41 -0200

    [UPD] ajuste psr2

--- src/Elements/ICMSIPI/Z0400.php
+++ src/Elements/ICMSIPI/Z0400.php
@@ -9,15 +9,15 @@ use \stdClass;
 /**
  * Elemento 0400 do Bloco 0
  *
- * Este registro tem por objetivo codificar os textos das diferentes 
- * naturezas  da operação/prestações discriminadas nos documentos fiscais. 
- * Esta codificação  e suas descrições são livremente criadas e mantidas 
+ * Este registro tem por objetivo codificar os textos das diferentes
+ * naturezas  da operação/prestações discriminadas nos documentos fiscais.
+ * Esta codificação  e suas descrições são livremente criadas e mantidas
  * pelo contribuinte.
- * 
- * Este registro não se refere a CFOP. Algumas empresas utilizam outra 
- * classificação  além das apresentadas nos CFOP. Esta codificação permite 
+ *
+ * Este registro não se refere a CFOP. Algumas empresas utilizam outra
+ * classificação  além das apresentadas nos CFOP. Esta codificação permite
  * informar estes agrupamentos próprios.
- * 
+ *
  * NOTA: usada a letra Z no nome da Classe pois os nomes não podem ser exclusivamente
  * numeréricos e também para não confundir os com elementos do bloco B
  */
