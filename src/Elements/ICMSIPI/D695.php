335d050cf74522a1093f24a42f3274530aba50bf
eduardorcsouza <eduardo.r.c.souza@gmail.com>
2019-05-08 15:08:31 -0300

    [UPD] Atualizado Elemento D695 e D696 para nfe21

--- src/Elements/ICMSIPI/D695.php
+++ src/Elements/ICMSIPI/D695.php
@@ -12,7 +12,64 @@ class D695 extends Element implements ElementInterface
     const LEVEL = 2;
     const PARENT = '';
 
-    protected $parameters = [];
+    protected $parameters = [
+        'COD_MOD' => [
+            'type' => 'numeric',
+            'regex' => '^(\d{3})$',
+            'required' => true,
+            'info' => 'Código do modelo do documento fiscal, conforme a Tabela 4.1.1.',
+            'format' => ''
+        ],
+        'SER' => [
+            'type' => 'numeric',
+            'regex' => '^(\d{3})$',
+            'required' => true,
+            'info' => 'Série do documento fiscal ',
+            'format' => ''
+        ],
+        'NRO_ORD_INI' => [
+            'type' => 'numeric',
+            'regex' => '^(\d{3})$',
+            'required' => true,
+            'info' => 'Número de ordem inicial ',
+            'format' => ''
+        ],
+        'NRO_ORD_FIN' => [
+            'type' => 'numeric',
+            'regex' => '^(\d{3})$',
+            'required' => true,
+            'info' => 'Número de ordem final ',
+            'format' => ''
+        ],
+        'DT_DOC_INI' => [
+            'type' => 'numeric',
+            'regex' => '^(\d{3})$',
+            'required' => true,
+            'info' => 'Data de emissão inicial dos documentos ',
+            'format' => ''
+        ],
+        'DT_DOC_FIN' => [
+            'type' => 'numeric',
+            'regex' => '^(\d{3})$',
+            'required' => true,
+            'info' => 'Data de emissão final dos documentos ',
+            'format' => ''
+        ],
+        'NOM_MEST' => [
+            'type' => 'numeric',
+            'regex' => '^(\d{3})$',
+            'required' => true,
+            'info' => 'Nome do arquivo Mestre de Documento Fiscal',
+            'format' => ''
+        ],
+        'CHV_COD_DIG' => [
+            'type' => 'numeric',
+            'regex' => '^(\d{3})$',
+            'required' => true,
+            'info' => 'Chave de codificação digital do arquivo Mestre de Documento Fiscal',
+            'format' => ''
+        ],
+    ];
 
     /**
      * Constructor
