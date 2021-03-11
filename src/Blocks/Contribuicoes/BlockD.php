5fa966873e04bf3a2f02cbd446934df285e0d93c
robmachado <linux.rlm@gmail.com>
2021-03-11 18:42:03 -0300

    complementação dos blocos EFD Contribuição

--- src/Blocks/Contribuicoes/BlockD.php
+++ src/Blocks/Contribuicoes/BlockD.php
@@ -12,5 +12,28 @@ use NFePHP\EFD\Common\BlockInterface;
  */
 final class BlockD extends Block implements BlockInterface
 {
-    public $elements = [];
+    public $elements = [
+        'd001' => ['class' => Elements\D001::class, 'level' => 1, 'type' => 'single'],
+        'd010' => ['class' => Elements\D010::class, 'level' => 2, 'type' => 'single'],
+        'd100' => ['class' => Elements\D100::class, 'level' => 3, 'type' => 'multiple'],
+        'd101' => ['class' => Elements\D101::class, 'level' => 4, 'type' => 'multiple'],
+        'd105' => ['class' => Elements\D105::class, 'level' => 4, 'type' => 'multiple'],
+        'd111' => ['class' => Elements\D111::class, 'level' => 4, 'type' => 'multiple'],
+        'd200' => ['class' => Elements\D200::class, 'level' => 3, 'type' => 'multiple'],
+        'd201' => ['class' => Elements\D201::class, 'level' => 4, 'type' => 'multiple'],
+        'd205' => ['class' => Elements\D205::class, 'level' => 4, 'type' => 'multiple'],
+        'd209' => ['class' => Elements\D209::class, 'level' => 4, 'type' => 'multiple'],
+        'd300' => ['class' => Elements\D300::class, 'level' => 3, 'type' => 'multiple'],
+        'd309' => ['class' => Elements\D309::class, 'level' => 4, 'type' => 'multiple'],
+        'd350' => ['class' => Elements\D350::class, 'level' => 3, 'type' => 'multiple'],
+        'd359' => ['class' => Elements\D359::class, 'level' => 4, 'type' => 'multiple'],
+        'd500' => ['class' => Elements\D500::class, 'level' => 3, 'type' => 'multiple'],
+        'd501' => ['class' => Elements\D501::class, 'level' => 4, 'type' => 'multiple'],
+        'd505' => ['class' => Elements\D505::class, 'level' => 4, 'type' => 'multiple'],
+        'd509' => ['class' => Elements\D509::class, 'level' => 4, 'type' => 'multiple'],
+        'd600' => ['class' => Elements\D600::class, 'level' => 3, 'type' => 'multiple'],
+        'd601' => ['class' => Elements\D601::class, 'level' => 4, 'type' => 'multiple'],
+        'd605' => ['class' => Elements\D605::class, 'level' => 4, 'type' => 'multiple'],
+        'd609' => ['class' => Elements\D609::class, 'level' => 4, 'type' => 'multiple'],
+    ];
 }
--- src/Blocks/Contribuicoes/BlockM.php
+++ src/Blocks/Contribuicoes/BlockM.php
@@ -12,5 +12,38 @@ use NFePHP\EFD\Common\BlockInterface;
  */
 final class BlockM extends Block implements BlockInterface
 {
-    public $elements = [];
+    public $elements = [
+        'm001' => ['class' => Elements\M001::class, 'level' => 1, 'type' => 'single'],
+        'm100' => ['class' => Elements\M100::class, 'level' => 2, 'type' => 'single'],
+        'm105' => ['class' => Elements\M105::class, 'level' => 3, 'type' => 'multiple'],
+        'm110' => ['class' => Elements\M110::class, 'level' => 3, 'type' => 'multiple'],
+        'm115' => ['class' => Elements\M115::class, 'level' => 4, 'type' => 'multiple'],
+        'm200' => ['class' => Elements\M200::class, 'level' => 2, 'type' => 'multiple'],
+        'm205' => ['class' => Elements\M205::class, 'level' => 3, 'type' => 'multiple'],
+        'm210' => ['class' => Elements\M210::class, 'level' => 3, 'type' => 'multiple'],
+        'm211' => ['class' => Elements\M211::class, 'level' => 3, 'type' => 'multiple'],
+        'm215' => ['class' => Elements\M215::class, 'level' => 4, 'type' => 'multiple'],
+        'm220' => ['class' => Elements\M220::class, 'level' => 4, 'type' => 'multiple'],
+        'm225' => ['class' => Elements\M225::class, 'level' => 5, 'type' => 'multiple'],
+        'm230' => ['class' => Elements\M230::class, 'level' => 4, 'type' => 'multiple'],
+        'm300' => ['class' => Elements\M300::class, 'level' => 2, 'type' => 'multiple'],
+        'm350' => ['class' => Elements\M350::class, 'level' => 2, 'type' => 'multiple'],
+        'm400' => ['class' => Elements\M400::class, 'level' => 2, 'type' => 'multiple'],
+        'm410' => ['class' => Elements\M410::class, 'level' => 3, 'type' => 'multiple'],
+        'm500' => ['class' => Elements\M500::class, 'level' => 2, 'type' => 'multiple'],
+        'm505' => ['class' => Elements\M505::class, 'level' => 3, 'type' => 'multiple'],
+        'm510' => ['class' => Elements\M510::class, 'level' => 3, 'type' => 'multiple'],
+        'm515' => ['class' => Elements\M515::class, 'level' => 4, 'type' => 'multiple'],
+        'm600' => ['class' => Elements\M600::class, 'level' => 2, 'type' => 'multiple'],
+        'm605' => ['class' => Elements\M605::class, 'level' => 3, 'type' => 'multiple'],
+        'm610' => ['class' => Elements\M610::class, 'level' => 3, 'type' => 'multiple'],
+        'm611' => ['class' => Elements\M611::class, 'level' => 4, 'type' => 'multiple'],
+        'm615' => ['class' => Elements\M615::class, 'level' => 4, 'type' => 'multiple'],
+        'm620' => ['class' => Elements\M620::class, 'level' => 4, 'type' => 'multiple'],
+        'm625' => ['class' => Elements\M625::class, 'level' => 5, 'type' => 'multiple'],
+        'm630' => ['class' => Elements\M630::class, 'level' => 4, 'type' => 'multiple'],
+        'm700' => ['class' => Elements\M700::class, 'level' => 2, 'type' => 'multiple'],
+        'm800' => ['class' => Elements\M800::class, 'level' => 2, 'type' => 'multiple'],
+        'm810' => ['class' => Elements\M810::class, 'level' => 3, 'type' => 'multiple'],
+    ];
 }
