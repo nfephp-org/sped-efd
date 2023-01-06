693a64a174ede4f21836ee01dcf11d674c5734e9
Gustavo Lidani <desenvolvimento3@smallsoft.com.br>
2023-01-06 08:13:47 -0300

    feat(0221): adicionado elemento 0221 ao Bloco 0

--- src/Blocks/ICMSIPI/Block0.php
+++ src/Blocks/ICMSIPI/Block0.php
@@ -19,6 +19,7 @@ use NFePHP\EFD\Common\BlockInterface;
  * @method Elements\Z0001 z0001(\stdClass $std) Constructor element 0001
  * @method Elements\Z0002 z0002(\stdClass $std) Constructor element 0002
  * @method Elements\Z0005 z0005(\stdClass $std) Constructor element 0005
+ * @method Elements\Z0015 z0015(\stdClass $std) Constructor element 0015
  * @method Elements\Z0100 z0100(\stdClass $std) Constructor element 0100
  * @method Elements\Z0150 z0150(\stdClass $std) Constructor element 0150
  * @method Elements\Z0175 z0175(\stdClass $std) Constructor element 0175
@@ -28,11 +29,13 @@ use NFePHP\EFD\Common\BlockInterface;
  * @method Elements\Z0206 z0206(\stdClass $std) Constructor element 0206
  * @method Elements\Z0210 z0210(\stdClass $std) Constructor element 0210
  * @method Elements\Z0220 z0220(\stdClass $std) Constructor element 0220
+ * @method Elements\Z0221 z0221(\stdClass $std) Constructor element 0221
  * @method Elements\Z0300 z0300(\stdClass $std) Constructor element 0300
  * @method Elements\Z0305 z0305(\stdClass $std) Constructor element 0305
  * @method Elements\Z0400 z0400(\stdClass $std) Constructor element 0400
  * @method Elements\Z0450 z0450(\stdClass $std) Constructor element 0450
  * @method Elements\Z0460 z0460(\stdClass $std) Constructor element 0460
+ * @method Elements\Z0500 z0500(\stdClass $std) Constructor element 0500
  * @method Elements\Z0600 z0600(\stdClass $std) Constructor element 0600
  */
 final class Block0 extends Block implements BlockInterface
@@ -54,6 +57,7 @@ final class Block0 extends Block implements BlockInterface
         'z0206' => ['class' => Elements\Z0206::class, 'level' => 3, 'type' => 'single'],
         'z0210' => ['class' => Elements\Z0210::class, 'level' => 3, 'type' => 'multiple'],
         'z0220' => ['class' => Elements\Z0220::class, 'level' => 3, 'type' => 'multiple'],
+        'z0221' => ['class' => Elements\Z0221::class, 'level' => 3, 'type' => 'multiple'],
         'z0300' => ['class' => Elements\Z0300::class, 'level' => 2, 'type' => 'multiple'],
         'z0305' => ['class' => Elements\Z0305::class, 'level' => 3, 'type' => 'single'],
         'z0400' => ['class' => Elements\Z0400::class, 'level' => 2, 'type' => 'multiple'],
